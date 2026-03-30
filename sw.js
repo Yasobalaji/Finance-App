8e41dea1062d449127e18c4b649dcc5a77ccaede
Yasobalaji <Yasobalaji_R@hotmail.com>
2026-03-31 02:29:38 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'fbs-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
